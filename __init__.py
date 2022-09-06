cf0ddccceddcd187db489947549455b5a44b0df6
franck403 <79372051+franck403@users.noreply.github.com>
2022-09-06 18:57:33 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
 def check(text):
     if not (text.isalpha() or text.isdigit() or text.isspace()):
-        print("false")
+        special_characters = "_"
         
+        if any(c in special_characters for c in text):
+            print("true")
+        else:
+            print("flase")        
     else:
         print("true")
